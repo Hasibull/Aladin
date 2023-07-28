32664609728b586d4272291786defece00fb26e2
Hasibull <hasibul.sbn@gmail.com>
2023-07-28 19:25:18 +0600

    code structure done

--- src/main.py
+++ src/main.py
@@ -1,3 +1,12 @@
-import tkinter
+from vosk import Model, KaldiRecognizer
+import pyaudio
 
+model = Model(r"paste path here")
+recognizer = KaldiRecognizer(model, 16000)
 
+mic = pyaudio.PyAudio()
+stream = mic.open(format=pyaudio.paInt16, channels=1,
+                  rate=16000, input=True, frames_per_buffer=8192)
+stream.start_stream()
+while True:
+    data = stream.read(4096)
